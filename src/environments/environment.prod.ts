c40e17b396c4a549889a582082d83a6f0360a1f8
apaolett <Antonello.Paoletti@lnf.infn.it>
2019-04-17 16:45:18 +0200

    day 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import {KeycloakConfig} from "keycloak-angular";
+
+const keycloakConfig: KeycloakConfig = {
+  url: 'https://idp.app-dev.infn.it/auth',
+  realm: 'infn',
+  clientId: 'infn-portal'
+};
+
 export const environment = {
-  production: true
+  production: true,
+  YOUTUBE_API_KEY: 'AIzaSyCtqVahdJySRfiS5RzubU-B0JwugsEIkSo',
+  YOUTUBE_API_URL: 'https://www.googleapis.com/youtube/v3',
+  keycloak: keycloakConfig,
 };
